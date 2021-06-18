5d59dc29919923b1716539024b0263dc028f323d
Matías Gayo <matigayito.mg@gmail.com>
2021-06-18 16:09:19 -0300

    cambios al play game

--- contracts/Lobby.sol
+++ contracts/Lobby.sol
@@ -9,19 +9,34 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 contract RockPaperScissors is Ownable{
-    
- 
+
+    using SafeMath for ui;
+
     struct Game{
         address payable player1;
         address payable player2;
         uint bet;
         bool isFull;
+        mapping (address => byte32) playerMove; 
+        mapping (address =>uint) playerTimer; 
     }
     Game[] games;
+
+    modifier isPlayable(string memory _word) {
+        require(keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("papel"))) ||
+        keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("tijera"))) || 
+        keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("piedra"))), "la palabra ingresada debe ser 'piedra', 'papel' o 'tijera'");
+        _;
+    }
     
     function createGame(address payable _host,uint _bet) public{
-        games.push(Game(_host,payable(address(0x0)),_bet,false));
+        Game game = new Game;
+        game.player1 = msg.sender;
+        game.bet = _bet;
+        game.isFull = false;
+        games.push(Game(game));
     }
+
     function findGame(uint _minBet, uint _maxBet, address payable _searchingPlayer) public{
         for(uint i = 0; i < games.length; i++){
             if(games[i].isFull == false && games[i].bet > _minBet && games[i].bet < _maxBet){
@@ -45,15 +60,22 @@ contract RockPaperScissors is Ownable{
          
          uint playerWiner = checkPlay(_movePlayer1,_movePlayer2);
          if(playerWiner == 0){ // esto es una idea nomas, no esta bien aplicada la recursividad
-             play(_movePlayer1,_movePlayer2,_game);
+             _game.player1.transfer(_game.bet.div(2));
+             _game.player2.transfer(_game.bet.div(2));
          }else if (playerWiner == 1){
              _game.player1.transfer(_game.bet);
          }else{ 
              _game.player2.transfer(_game.bet);
          }
-         
-     }
-     
+    }
+
+    function playMove(Game _game, string memory _move, string memory _keyword) public isPlayable(_move) {
+        string memory moveKeyword = _move + " " + _keyword;
+        bytes32 moveEncrypted =  keccak256(abi.encodePacked((moveKeyword)));
+        _game.playerMove[msg.sender] = moveEncrypted;
+        // hay que setear el timer
+    }
+
      function checkPlay(uint _movePlayer1, uint _movePlayer2) internal pure returns (uint winer) {
          // 0 = Rock
          // 1 = Paper
