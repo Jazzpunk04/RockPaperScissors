f4f3320daac2eb35ba2b9eea76c1bafad5fe1bb0
lucasBarbeito <lucas.barbeito@ing.austral.edu.ar>
2021-06-27 02:34:10 -0300

    creeria que esta terminado

--- contracts/Lobby.sol
+++ contracts/Lobby.sol
@@ -9,42 +9,37 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 contract RockPaperScissors is Ownable{
-
-    using SafeMath for ui;
-
+    
+ 
     struct Game{
         address payable player1;
         address payable player2;
         uint bet;
         bool isFull;
-        mapping (address => byte32) playerMove; 
-        mapping (address =>uint) playerTimer; 
     }
-    Game[] games;
-
-    modifier isPlayable(string memory _word) {
-        require(keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("papel"))) ||
-        keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("tijera"))) || 
-        keccak256(abi.encodePacked((_word))) == keccak256(abi.encodePacked(("piedra"))), "la palabra ingresada debe ser 'piedra', 'papel' o 'tijera'");
-        _;
+    constructor()  payable {
+        
     }
     
-    function createGame(address payable _host,uint _bet) public{
-        Game game = new Game;
-        game.player1 = msg.sender;
-        game.bet = _bet;
-        game.isFull = false;
-        games.push(Game(game));
+    modifier isPlayer(Game memory _game){
+        require (_game.player1 == msg.sender && _game.player2 == msg.sender);
+        _;
     }
-
-    function findGame(uint _minBet, uint _maxBet, address payable _searchingPlayer) public{
+    Game[] games;
+    
+    function createGame(address payable _host,uint _bet) public{
+        games.push(Game(_host,payable(address(0x0)),_bet,false));
+    }
+    
+    function findGame(uint _minBet, uint _maxBet, address payable _searchingPlayer) public {
         for(uint i = 0; i < games.length; i++){
             if(games[i].isFull == false && games[i].bet > _minBet && games[i].bet < _maxBet){
                 joinGame(games[i], _searchingPlayer);
                 Game memory joinedGame = games[i];
                 games[i] = games[games.length-1];
                 delete games[games.length-1];
-                // return joinedGame;
+                //returns (address payable player1, address payable player2, uint bet, bool isFull) 
+                // return (joinedGame.player1,joinedGame.player2,joinedGame.bet,joinedGame.isFull);
             }
         }
     }
@@ -55,44 +50,79 @@ contract RockPaperScissors is Ownable{
         _game.player2 = joinedPlayer;
     }
     
-     function play(uint  _movePlayer1, uint  _movePlayer2, Game memory _game) public{
-         require(_movePlayer1 >=0 && _movePlayer1 <=2 && _movePlayer2 >=0 && _movePlayer2 <=2);
+    function play(string memory _P1Move, string memory _P2Move, Game memory _game) public isPlayer(_game){
+        
+         uint p1HashedMove = hashMove(_P1Move);
+         uint p2HashedMove = hashMove(_P2Move);
          
-         uint playerWiner = checkPlay(_movePlayer1,_movePlayer2);
-         if(playerWiner == 0){ // esto es una idea nomas, no esta bien aplicada la recursividad
-             _game.player1.transfer(_game.bet.div(2));
-             _game.player2.transfer(_game.bet.div(2));
-         }else if (playerWiner == 1){
-             _game.player1.transfer(_game.bet);
-         }else{ 
-             _game.player2.transfer(_game.bet);
-         }
+         require(p1HashedMove == uint(keccak256(abi.encodePacked(("rock")))) || p1HashedMove == uint(keccak256(abi.encodePacked(("paper")))) || p1HashedMove == uint(keccak256(abi.encodePacked(("scissors")))), 
+         "The play must be 'rock', 'paper' or 'scissors' ");
+         
+         require(p2HashedMove == uint(keccak256(abi.encodePacked(("rock")))) || p2HashedMove == uint(keccak256(abi.encodePacked(("paper")))) || p2HashedMove == uint(keccak256(abi.encodePacked(("scissors")))), 
+         "The play must be 'rock', 'paper' or 'scissors' ");
+         
+         payWiner(p1HashedMove,p2HashedMove,_game);
+        
+       
     }
-
-    function playMove(Game _game, string memory _move, string memory _keyword) public isPlayable(_move) {
-        string memory moveKeyword = _move + " " + _keyword;
-        bytes32 moveEncrypted =  keccak256(abi.encodePacked((moveKeyword)));
-        _game.playerMove[msg.sender] = moveEncrypted;
-        // hay que setear el timer
-    }
-
-     function checkPlay(uint _movePlayer1, uint _movePlayer2) internal pure returns (uint winer) {
-         // 0 = Rock
-         // 1 = Paper
-         // 2 = Scissors
+    
+     function hashMove(string memory _move) internal pure returns (uint hashedMove){
+         uint  _hashedMove = uint(keccak256(abi.encodePacked(_move)));
+         return (_hashedMove);
+         
+     }
+     
+     function checkWiner(uint _movePlayer1, uint _movePlayer2) internal pure returns (uint winer) {
+         // keccak256 hash generator: https://sita.app/keccak256-hash-generator
+         // rock:       hexa: 10977e4d68108d418408bc9310b60fc6d0a750c63ccef42cfb0ead23ab73d102  => uint: 7504671191028674570649298396935315689168425980718395638832996684551825903874
+         // paper:      hexa: ea923ca2cdda6b54f4fb2bf6a063e5a59a6369ca4c4ae2c4ce02a147b3036a21  => uint: 106099584733913018769790338291824602800549410377166706717988945967699957344801
+         // scissors:   hexa: 389a2d4e358d901bfdf22245f32b4b0a401cc16a4b92155a2ee5da98273dad9a  => uint: 25601926655740021617648276659444374542935864018550230109551161942298075180442
+         
+         uint rockUint  = 7504671191028674570649298396935315689168425980718395638832996684551825903874;
+         uint paperUint = 106099584733913018769790338291824602800549410377166706717988945967699957344801;
+         uint scissorsUint = 25601926655740021617648276659444374542935864018550230109551161942298075180442;
+         
          //Tie
-         if(_movePlayer1 == 0 && _movePlayer2 == 0 || _movePlayer1 == 1 && _movePlayer2 == 1 
-            || _movePlayer1 == 2 && _movePlayer2 == 2){
+         if(_movePlayer1 == rockUint && _movePlayer2 == rockUint || _movePlayer1 == paperUint && _movePlayer2 == paperUint 
+            || _movePlayer1 == scissorsUint && _movePlayer2 == scissorsUint){
              return (0); // its a tie
          }
          //Player 1 Wins
-         if(_movePlayer1 == 0 && _movePlayer2 == 2 ||
-            _movePlayer1 == 1 && _movePlayer2 == 0 ||
-            _movePlayer1 == 2 && _movePlayer2 == 1){
+         if(_movePlayer1 == rockUint && _movePlayer2 == scissorsUint ||
+            _movePlayer1 == paperUint && _movePlayer2 == rockUint ||
+            _movePlayer1 == scissorsUint && _movePlayer2 == paperUint){
                 
              return (1); //  player 1 Wins
              
          }else return(2);//  player 2 Wins
          
      }
+     
+    
+    function payWiner(uint  _movePlayer1, uint  _movePlayer2, Game memory _game) internal  {
+
+         uint playerWiner = checkWiner(_movePlayer1,_movePlayer2);
+         
+         if(playerWiner == 0){
+             _game.player1.transfer(_game.bet);
+             _game.player2.transfer(_game.bet);
+         }else if (playerWiner == 1){
+             _game.player1.transfer(2*_game.bet);
+         }else{ 
+             _game.player2.transfer(2*_game.bet);
+         }
+         
+     }
+     
+     
+     function getGames() public view returns (Game[] memory){
+        return games;
+    }
 }   
+
+//    function playMove(Game _game, string memory _move, string memory _keyword) public isPlayable(_move) {
+//         string memory moveKeyword = _move + " " + _keyword;
+//         bytes32 moveEncrypted =  keccak256(abi.encodePacked((moveKeyword)));
+//         _game.playerMove[msg.sender] = moveEncrypted;
+//         // hay que setear el timer
+//     }
